d01c80c2f2a4112fd3a69b7faba3a1428c77e204
yousefmohamed35 <146759411+yousefmohamed35@users.noreply.github.com>
2024-03-04 10:11:11 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ToDoApp());
+}
+
+class ToDoApp extends StatelessWidget {
+  const ToDoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
