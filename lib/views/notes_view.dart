c1b27899253a762f2ed955c96d96d1dc4dad0462
yousefmohamed35 <146759411+yousefmohamed35@users.noreply.github.com>
2024-03-04 15:44:35 +0200

    add note model bottom sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todoapp/widget/add_note_bottom_sheet.dart';
 import 'package:todoapp/widget/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
@@ -6,14 +7,25 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      floatingActionButton: 
-      FloatingActionButton(
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
         backgroundColor: Color.fromARGB(255, 108, 247, 212),
-       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(500),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(500),
         ),
-        onPressed: (){},child: const Icon(Icons.add,color: Colors.black,),),
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) {
+              return AddNoteBottomSheet();
+            },
+          );
+        },
+        child: const Icon(
+          Icons.add,
+          color: Colors.black,
+        ),
+      ),
       body: const NotesViewBody(),
     );
   }
