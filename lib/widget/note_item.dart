cb712db9ad28c111c3ccd4b2768d6fba003061a7
yousefmohamed35 <146759411+yousefmohamed35@users.noreply.github.com>
2024-03-07 00:19:23 +0200

    display data part two

--- lib/widget/note_item.dart
+++ lib/widget/note_item.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:todoapp/models/note_model.dart';
 import 'package:todoapp/views/edit_view.dart';
 
 class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
-
+  const NoteItem({super.key, required this.note});
+  final NoteModel note;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -21,21 +22,21 @@ class NoteItem extends StatelessWidget {
       child: Container(
         padding: const EdgeInsets.only(top: 24, bottom: 24, left: 24),
         decoration: BoxDecoration(
-          color: Colors.yellow,
+          color: Color(note.color),
           borderRadius: BorderRadius.circular(16),
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             ListTile(
-              title: const Text('Flutter Tips'),
+              title: Text(note.title),
               titleTextStyle: const TextStyle(
                   color: Colors.black,
                   fontSize: 24,
                   fontWeight: FontWeight.bold),
-              subtitle: const Padding(
-                padding: EdgeInsets.only(top: 16.0, bottom: 16),
-                child: Text('Build your career with tharwat samy'),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16.0, bottom: 16),
+                child: Text(note.subTitle),
               ),
               subtitleTextStyle:
                   TextStyle(color: Colors.black.withOpacity(.5), fontSize: 18),
@@ -51,7 +52,7 @@ class NoteItem extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.only(right: 24),
               child: Text(
-                'May 21, 2021',
+                '${DateTime.parse(note.date).day.toString()}-${DateTime.parse(note.date).month.toString()}',
                 style: TextStyle(
                     color: Colors.black.withOpacity(.5), fontSize: 16),
               ),
